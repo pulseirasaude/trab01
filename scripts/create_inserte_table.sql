068a5d3360caae339ffd89b339b6f71919b9d1a5
Leonardo Voltolin de Sena <leosena1995@gmail.com>
2018-11-29 02:07:04 -0200

    Update create and insert script

--- scripts/create_inserte_table.sql
+++ scripts/create_inserte_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE PESSOA (
   DATA_CADASTRO DATE,
   PRIMARY KEY (ID)
 );
+
 CREATE TABLE PESSOA_FISICA (
   ID SERIAL,
   CPF VARCHAR(14),
@@ -43,27 +44,17 @@ CREATE TABLE CATEGORIA_CNH(
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE CNH(
-  ID SERIAL,
-  NUMERO VARCHAR(45),
-  DATA_VENCIMENTO DATE,
-  ID_CATEGORIA_CNH INT,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (ID_CATEGORIA_CNH)
-  REFERENCES CATEGORIA_CNH (ID)
-);
-
 CREATE TABLE MOTORISTA (
   ID SERIAL,
-  CNH VARCHAR(11),
+  NUMERO_CNH VARCHAR(11),
   DATA_VENCIMENTO DATE,
-  ID_CNH INT,
+  ID_CATEGORIA_CNH INT,
   ID_FUNCIONARIO INT NOT NULL,
   PRIMARY KEY (ID),
   FOREIGN KEY (ID_FUNCIONARIO)
   REFERENCES FUNCIONARIO (ID),
-  FOREIGN KEY (ID_CNH)
-  REFERENCES CNH (ID)
+  FOREIGN KEY (ID_FUNCIONARIO)
+  REFERENCES CATEGORIA_CNH(ID)
 );
 
 CREATE TABLE TIPO_CONTATO (
@@ -101,7 +92,7 @@ CREATE TABLE CIDADE(
 
 CREATE TABLE BAIRRO (
   ID SERIAL,
-  NOME_BAIRRO VARCHAR(45),
+  NOME VARCHAR(45),
   ID_CIDADE INT,
   PRIMARY KEY (ID),
   FOREIGN KEY (ID_CIDADE)
@@ -130,26 +121,36 @@ CREATE TABLE ENDERECO (
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE CATEGORIA (
+CREATE TABLE ENDERECO_PESSOA(
   ID SERIAL,
-  NOME VARCHAR(45),
+  ID_ENDERECO INT NOT NULL,
+  ID_PESSOA INT NOT NULL,
+  FOREIGN KEY (ID_ENDERECO)
+  REFERENCES ENDERECO(ID),
+  FOREIGN KEY (ID_PESSOA)
+  REFERENCES PESSOA(ID),
+  PRIMARY KEY (ID)
+);
+CREATE TABLE CATEGORIA_VEICULO (
+  ID SERIAL,
+  CATEGORIA VARCHAR(45),
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE MARCA (
-  ID INT,
+  ID SERIAL,
   MARCA VARCHAR(45),
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE EIXO(
-  ID INT,
-  EIXO INT,
+  ID SERIAL,
+  QUANTIDADE_EIXO INT,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE COR  (
-  ID INT,
+  ID SERIAL,
   COR VARCHAR(45),
   PRIMARY KEY (ID)
 );
@@ -191,14 +192,14 @@ CREATE TABLE VEICULO (
   ID SERIAL,
   NOME VARCHAR(45),
   CHASSI VARCHAR(45),
-  PLACA VARCHAR(7),
+  PLACA VARCHAR(8),
   ID_STATUS INT,
   ID_COMBUSTIVEL INT,
   ID_CATEGORIA INT NOT NULL,
   ID_MODELO INT NOT NULL,
   PRIMARY KEY (ID),
   FOREIGN KEY (ID_CATEGORIA)
-  REFERENCES CATEGORIA (ID),
+  REFERENCES CATEGORIA_VEICULO (ID),
   FOREIGN KEY (ID_MODELO)
   REFERENCES MODELO (ID),
   FOREIGN KEY (ID_STATUS)
@@ -208,7 +209,7 @@ CREATE TABLE VEICULO (
 );
 
 
-CREATE TABLE MOTORISTA_DIRIGE_VEICULO(
+CREATE TABLE MOTORISTA_VEICULO(
   ID SERIAL,
   DATA_INICIO DATE,
   DATA_FIM DATE,
@@ -229,7 +230,7 @@ CREATE TABLE STATUS_TRANSPORTE(
 
 CREATE TABLE TIPO_CARGA (
   ID SERIAL,
-  DESCRICAO VARCHAR(45),
+  TIPO VARCHAR(45),
   PRIMARY KEY (ID)
 );
 
@@ -262,116 +263,228 @@ CREATE TABLE TRANSPORTE (
   REFERENCES TIPO_CARGA(ID)
 );
 
-insert into CATEGORIA (ID,NOME) values
-	(1,'Caminhao 2 eixos'),
-	(2,'Caminhao 3 eixos'),
-	(3,'Caminhao 4 eixos'),
-	(4,'Mini Van'),
-	(5,'Seda');
-    
-insert into MODELO (MODELO, MARCA, ANO, EIXO, PESO, ALTURA, LARGURA, COMPRIMENTO, COR) values
-	('1414','Mercedes',1995,2,6000.00,3.00,2.80,4.20,'azul'),
-	('1515','Mercedes',1999,2,6000.00,3.00,3.00,4.40,'amarelo'),
-	('710','Mercedes',2000,2,5600.00,3.00,2.80,4.00,'laranja'),
-	('spin','Chevrolet',2008,2,3000.00,2.60,2.40,3.40,'branco'),
-	('Vectra','Ford',2005,2,2500.00,1.80,2.10,3.00,'prata');
-    
-insert into FUNCIONARIO (NOME, CPF, CARGO, SALARIO, SEXO, DATA_CADASTRO, DATA_NASCIMENTO) values
-	('Marcos','17131518042','MOTORISTA',3000.00,'M','2015-05-01','1980-09-19'),
-    ('Douglas','04418273069','MOTORISTA',2200.00,'M','2016-06-15','1985-05-23'),
-    ('Mario','04635715094','MOTORISTA',3500.00,'M','2015-07-12','1989-07-24'),
-    ('Cleber','04598206055','MOTORISTA',2700.00,'M','2016-01-10','1987-06-16'),
-    ('Jonathan','19793260025','MOTORISTA',4000.00,'M','2014-02-20','1990-01-12');
+INSERT INTO PESSOA (NOME, DATA_CADASTRO) 
+VALUES ('Thales Real', '2018-01-12'),
+	   ('Ryan Augusto da Mata', '2015-06-20'),
+	   ('Tomás da Cunha', '2016-08-08'),
+	   ('Cauã Pires', '2017-11-04'),
+	   ('Elias Peixoto', '2018-07-16'),
+	   ('Gustavo Araújo', '2017-05-05'),
+	   ('Davi e Helena Entulhos ME', '2018-07-18'),
+	   ('Emilly e Rebeca Padaria Ltda', '2017-04-13'),
+	   ('Teresinha e Esther Adega Ltda', '2016-08-09'),
+	   ('Luzia e Malu Telecomunicações ME', '2015-01-08');
+	   
 
-insert into CLIENTE (NOME, TIPO_CLIENTE,CNPJ, CPF, DATA_CADASTRO) values 	
-		('Jorge', 'A', '01.214.339/0001-53', '053.287.330-02', '2018-05-30'),
-		('Carlos', 'A', '33.457.141/0001-97', '641.167.660-70', '2018-07-25'),
-        ('Mateus', 'A', '76.711.710/0001-91', '317.281.670-27', '2018-04-19'),
-        ('Joao', 'A', '48.568.105/0001-78', '798.916.160-53', '2018-01-06'),
-        ('Douglas', 'A', '33.445.900/0001-00', '467.370.900-44', '2018-10-20');
+INSERT INTO PESSOA_FISICA (CPF, DATA_NASCIMENTO, SEXO, ID_PESSOA)
+VALUES ('240.211.364-28', '1996-07-13', 'M', 1),
+	   ('245.389.770-55', '1996-04-19', 'M', 2),
+	   ('124.664.660-90', '1990-10-19', 'M', 3),
+	   ('613.204.047-15', '1992-12-25', 'M', 4),
+	   ('805.153.783-13', '1992-01-18', 'M', 5),
+	   ('967.041.166-13', '1989-07-20', 'M', 6);
+	   
+INSERT INTO PESSOA_JURIDICA (CNPJ, RAZAO_SOCIAL, ID_PESSOA)
+VALUES ('03.845.897/0001-42', 'Davi e Helena Entulhos ME', 7),
+	   ('61.208.112/0001-27', 'Emilly e Rebeca Padaria Ltda', 8),
+	   ('46.539.911/0001-92', 'Teresinha e Esther Adega Ltda', 9),
+	   ('39.053.788/0001-30', 'Luzia e Malu Telecomunicações ME', 10);
 
-    
-insert into CONTATO (TELEFONE_01, TELEFONE_02, TELEFONE_03, EMAIL) values 
-	('99921-3881','3352-2470','99918-1430','contatofuncionario@gmail.com'),
-    ('99944-3276','3308-1470','99821-0405','contatofuncionario@hotmail.com'),
-    ('99933-4452','3321-5567','99916-2150','contatocliente@gmail.com'),
-    ('99905-1404','3364-5819','99914-2890','contatocliente@hotmail.com'),
-    ('99951-1211','3356-7080','99911-2130','emailcliente@outlook.com');
-    
-insert into CONTATO_CLIENTE (ID_CONTATO, ID_CLIENTE) values 
-	(3,1),
-    (4,2),
-    (5,3),
-    (3,4),
-    (4,5);
-    
-insert into CONTATO_FUNCIONARIO (ID_CONTATO, ID_FUNCIONARIO) values 
-	(1,1),
-    (2,2),
-    (2,3),
-    (5,4),
-    (1,5);
+INSERT INTO FUNCIONARIO (CARGO, SALARIO, ID_PESSOA_FISICA)
+VALUES ('MOTORISTA', 3300.00, 1),
+	   ('GERENTE DE TRANSPORTE', 5000.00, 2),
+	   ('GERENTE DE VEICULO', 4200.00, 3),
+	   ('MOTORISTA', 2200.00, 4),
+	   ('MOTORISTA', 3500.00, 5),
+	   ('MOTORISTA', 3000.00, 6);
 
-insert into MOTORISTA (CNH, DATA_VENCIMENTO, CATEGORIA_CNH, ID_FUNCIONARIO) values
-	(123456,'2018-10-20','E',1),
-	(234567,'2020-02-26','D',2),
-	(345678,'2021-05-15','E',3),
-	(456789,'2022-06-02','AB',4),
-	(567891,'2021-06-05','D',5);
+INSERT INTO CATEGORIA_CNH (CATEGORIA)
+VALUES ('A'),
+	   ('B'),
+	   ('C'),
+	   ('D'),
+	   ('AB'),
+	   ('AC'),
+	   ('AD'),
+	   ('BC'),
+	   ('BD'),
+	   ('CD');
 
-insert into VEICULO (NOME, CHASSI, PLACA, STATUS, COMBUSTIVEL, ID_CATEGORIA, ID_MODELO, ID_MOTORISTA) values 
-	('Azulao','12345','MKV1010','em uso','Disel', 1, 1, 1),
-	('Amarelao','23456','MKV1011','disponivel','Disel', 2, 2, 2),
-	('Laranjao','34567','MKV1012','disponivel','Disel', 3, 3, 3),
-	('mini','45678','MKV1013','em uso','Gasolina', 4, 4, 4),
-	('vectra','56789','MKV1014','em uso','Alcool', 5, 5, 5);
 
-insert into FINANCIAMENTO (VALOR_PARCELA, PARCELAS_TOTAIS, PARCELAS_PAGAS, FINANCIADOR, ID_VEICULO) values 
-	(800.00,60,22,'Santander',1),
-	(1000.00,48,40,'Banestes',2),
-	(1500.00,48,10,'ITAU',3),
-	(890.00,60,35,'Santander',4),
-	(550.00,48,20,'ITAU',5);
+INSERT INTO MOTORISTA (NUMERO_CNH, DATA_VENCIMENTO, ID_CATEGORIA_CNH, ID_FUNCIONARIO)
+VALUES ('65137192579', '2020-10-10', 6, 1),
+       ('05808723119', '2022-05-10', 7, 4),
+	   ('46569685935', '2019-09-10', 8, 5),
+	   ('15671759904', '2025-09-19', 9, 6);
+
+INSERT INTO TIPO_CONTATO (TIPO)
+VALUES ('EMAIL'),
+	   ('TELEFONE'),
+	   ('CELULAR');
+
+
+INSERT INTO CONTATO (CONTATO, ID_TIPO, ID_PESSOA)
+VALUES ('027999337259', 3, 1),
+	   ('thales@gmail.com', 1, 1),
+	   ('08929780426', 2, 2),
+	   ('ryan@hotmail.com', 1, 2),
+	   ('tomascunha@gmail.com', 1, 3),
+	   ('089994186619', 3, 3),
+	   ('04725870760', 2, 4),
+	   ('caua@outlook.com', 1, 4),
+	   ('047996257362', 3, 5),
+	   ('eliaspeixoto@hotmail.com', 1, 5),
+	   ('gustavoaraujo@hotmail.com', 1, 6),
+	   ('06828206226', 2, 6),
+	   ('contato@dhentulhos.com', 1, 7),
+	   ('068987285886', 3, 7),
+	   ('contato@erparadia.com', 1, 8),
+	   ('09139023180', 2, 8),
+	   ('contato@teadega.com', 1, 9),
+	   ('091981722332', 3, 9),
+	   ('contato@lmtelecomunicacao.com', 1, 10),
+	   ('09625574434', 2, 10);
+
+INSERT INTO ESTADO (SIGLA,NOME) 
+VALUES ('ES', 'ESPIRITO SANTO'),
+	   ('RJ', 'RIO DE JANEIRO'),
+	   ('MG','MINAS GERAIS'),
+	   ('SP','SÃO PAULO');
+	   
+INSERT INTO CIDADE(NOME, ID_ESTADO)
+VALUES ('VILA VELHA',1),
+	   ('VITORIA',1),
+	   ('SAO PAULO',4),
+	   ('BELO HORIZONTE', 3),
+	   ('CAGO FRIO', 2);
+	   
+INSERT INTO BAIRRO (NOME,  ID_CIDADE) 
+VALUES ('GUARANHUS', 1),
+	   ('SANTA MONICA POPULAR',1),
+	   ('JARDIM EUROPA', 3),
+	   ('JARDIM CAMBURI', 2),
+	   ('PAMPULHA',4),
+	   ('CENTRO',5);
+	   
+INSERT INTO LOGRADOURO (ABREVIACAO,NOME)
+VALUES ('R','RUA'),
+	   ('AV', 'AVENIDA');
 	
-insert into ENDERECO (ENDERECO, LOGRADOURO, NUMERO, CEP, COMPLEMENTO, CIDADE, ESTADO) values
-	('Augusto Ruschi','avena',12,'29164-830','casa','serra','es'),
-	('Brasil','avena',45,'29164-495','','serra','es'),
-	('Antônio Tironi','beco',08,'29164-637','','serra','es'),
-	('Angélica Paulina da Cruz','alameda',23,'29150-553','','cariacica','es'),
-	('Adilson Coelho','rua',102,'29150-533','casa','cariacica','es');
-    
-insert into ENDERECO_CLIENTE (ID_ENDERECO, ID_CLIENTE) values 
-	(1,1),
-    (2,2),
-    (3,3),
-    (4,4),
-    (5,5);
+INSERT INTO ENDERECO (ENDERECO, NUMERO, CEP, COMPLEMENTO, ID_ESTADO, ID_LOGRADOURO)
+VALUES ('Rua Jorge Amado', 281, '97065-211', 'Sala 502', 1, 1),
+	   ('Avenida Moacir Braga Coutinho', 1500, '68905-820', 'Galpão 3', 2, 2),
+	   ('Rua Perimetral Quatro', 140, '68905-820', 'Apto. 802', 3, 1),
+	   ('Rua José Silvério da Silva', 3000, '74953-610', 'Apto. 1002', 4, 1),
+	   ('Rua Antônio Escorsin', 281, '64033-550', 'Apto. 202', 1, 1),
+	   ('Rua José Silvério da Silva', 4254, '74730515', 'Sala 21',2, 1),
+	   ('Rua Pedro Ardêmio Hilgert', 123, '92310-560', 'Hangar 5',3, 1),
+	   ('Rua Ary Lopes Moreira', 2837, '27524-112', 'Galpão 66',4, 1),
+	   ('Rua Cristalina', 1928, '26298-213', 'Apto. 304',1, 1),
+	   ('Rua Joaquim Mesquita Filho', 183, '58037-205', 'Sala 111', 2, 1);
 
-insert into ENDERECO_FUNCIONARIO (ID_ENDERECO, ID_FUNCIONARIO) values 
-	(3,1),
-    (4,2),
-    (5,3),
-    (3,4),
-    (4,5);
+INSERT INTO ENDERECO_PESSOA (ID_ENDERECO, ID_PESSOA)
+VALUES 	(1, 1),
+		(2, 2),
+		(3, 3),
+		(4, 4),
+		(5, 5),
+		(6, 6),
+		(7, 7),
+		(9, 9),
+		(10, 10);
 
-insert into SEGURO (STATUS, DATA_VENCIMENTO, FIM_CONTRATO, ID_VEICULO) values 
-	('ativo','2018-08-25','2022-05-05',1),
-    ('desativo','2019-12-14','2026-01-18',2),
-    ('ativo','2020-06-27','2026-09-10',3),
-    ('desativo','2016-12-01','2018-10-10',4),
-    ('ativo','2022-09-09','2028-04-03',5);
-    
-insert into ENDERECO_SERVICO (TIPO, ID_ENDERECO) values
-	('D', 1),
-    ('O', 2),
-    ('O', 3),
-    ('D', 4),
-    ('O', 5);
-    
-insert into SERVICO (STATUS, VALOR_CONTRATO, DATA_INICIO, DATA_FIM, ID_CLIENTE, ID_FUNCIONARIO, ID_VEICULO, ID_ENDERECO_ORIGEM, ID_ENDERECO_DESTINO) values
-	('CONCLUDIO', 7000.00, '2016-03-14', '2018-03-14', 5, 5, 5, 2, 4),
-    ('EM ANDAMENTO', 5000.00, '2017-10-10', '2018-10-10', 4, 4, 4, 3, 4),
-    ('CONCLUDIO', 6000.00, '2017-02-25', '2018-02-25', 3, 3, 3, 5, 1),
-    ('CONCLUDIO', 4000.00, '2017-01-10', '2018-01-10', 2, 2, 2, 3, 4),
-    ('EM ANDAMENTO', 10000.00, '2018-08-20', '2020-08-20', 1, 1, 1, 2, 1);
-    
+INSERT INTO MARCA (MARCA) 
+VALUES ('Mercedes-Benz'), 
+	   ('Ford'), 
+	   ('Volkswagen'), 
+	   ('Volvo'), 
+	   ('Scania');
+	   
+INSERT INTO STATUS_VEICULO (STATUS) 
+VALUES ('DISPONIVEL'), 
+	   ('MANUTENCAO'), 
+	   ('EM TRANSPORTE');
+	   
+INSERT INTO STATUS_TRANSPORTE (STATUS) 
+VALUES ('CONCLUIDO'), 
+	   ('EM ANDAMENTO'), 
+	   ('CANCELADO'), 
+	   ('SOLICITADO');
+	   
+INSERT INTO COMBUSTIVEL (COMBUSTIVEL) 
+VALUES ('GASOLINA'), 
+	   ('ALCOOL'), 
+	   ('GAS'),
+	   ('DIESEL'),
+	   ('FLEX');
+
+INSERT INTO COR (COR) 
+VALUES ('VERMELHO'), 
+	   ('AZUL'), 
+	   ('VERDE'), 
+	   ('PRETO'), 
+	   ('BRANCO'), 
+	   ('PRATA'), 
+	   ('AMARELO'), 
+	   ('LARANJA'), 
+	   ('CINZA');
+
+INSERT INTO EIXO (QUANTIDADE_EIXO) 
+VALUES (2), 
+	   (3),
+	   (4), 
+	   (6);
+
+INSERT INTO CATEGORIA_VEICULO (CATEGORIA)
+VALUES ('CARRO'),
+	   ('CAMINHAO'),
+	   ('CARRETA'),
+	   ('ONIBUS'),
+	   ('VAN');
+	   
+INSERT INTO TIPO_CARGA (TIPO)
+VALUES ('CONSTRUCAO'),
+	   ('FRIGORIFICA'),
+	   ('VEICULO'),
+	   ('FRAGIL'),
+	   ('MEDICAMENTOS'),
+	   ('MUDANCA'),
+	   ('ENCOMENDAS');
+	   
+INSERT INTO MODELO (MODELO, ANO, PESO, ALTURA, LARGURA, COMPRIMENTO, ID_MARCA, ID_EIXO, ID_COR)
+VALUES ('Sable LS 3.0 V6', '2015', 16000.00, 2.6, 4.4, 18.15, 1, 2, 1),
+	   ('Javali 3.0', '2016', 25000.00, 2.6, 4.4, 18.15, 2, 3, 2),
+	   ('SE-500', '2014', 14000.00, 2.6, 4.4, 18.15, 1, 1, 3),
+	   ('T-100 3.4 V6', '2012', 16000.00, 2.6, 4.4, 18.15, 3, 1, 4),
+	   ('Master 4.0', '2011', 27000.00, 2.6, 4.4, 18.15, 3, 3, 5),
+	   ('Esprit S-4 2.2 16V', '2016', 18000.00, 2.6, 4.4, 18.15, 5, 2, 6),
+	   ('Previa LX 2.5', '2015', 20000.00, 2.6, 4.4, 18.15, 4, 2, 7),
+	   ('AUMARK 3.50AK', '2011', 22000.00, 2.6, 4.4, 18.15, 1, 3, 8),
+	   ('9000 CD', '2011', 22000.00, 2.6, 4.4, 18.15, 1, 3, 9),
+	   ('PANTANAL 3.0', '2013', 14000.00, 2.6, 4.4, 18.15, 2, 2, 4);
+
+INSERT INTO VEICULO (NOME, CHASSI, PLACA, ID_STATUS, ID_COMBUSTIVEL, ID_CATEGORIA, ID_MODELO)
+VALUES ('.', '9UK64ED50D0015277', 'MUN-6778', 2, 1, 2, 1),
+       ('.', '9UK64ED50D0015280', 'MZY-2035', 2, 2, 2, 2),
+	   ('.', '9UK64ED50D0015294', 'AJH-3703', 1, 3, 2, 3),
+	   ('.', '9UK64ED50D0015313', 'MQM-5362', 1, 4, 2, 4),
+	   ('.', '9UK64ED50D0015327', 'NDS-5037', 1, 5, 1, 5),
+	   ('.', '9UK64ED50D0015330', 'LXT-2999', 1, 1, 3, 6),
+	   ('.', '9UK64ED50D0015344', 'NAK-6648', 3, 2, 3, 7),
+	   ('.', '9UK64ED50D0015358', 'HUC-4058', 3, 3, 2, 8),
+	   ('.', '9UK64ED50D0015361', 'MZQ-6007', 3, 4, 2, 9),
+	   ('.', '9UK64ED50D0015473', 'HQE-4878', 3, 5, 2, 10);
+
+INSERT INTO MOTORISTA_VEICULO(DATA_INICIO, DATA_FIM, ID_MOTORISTA, ID_VEICULO)
+VALUES ('2018-10-15', '2018-10-30', 1, 7),
+	   ('2018-09-1', '2018-09-07', 2, 8),
+	   ('2018-10-7', '2018-10-21', 3, 9),
+	   ('2018-11-1', '2018-11-14', 4, 10);
+
+INSERT INTO TRANSPORTE (VALOR_CONTRATO, DATA_INICIO, DATA_FIM, ID_STATUS, ID_TIPO_CARGA, ID_CLIENTE, ID_FUNCIONARIO, ID_VEICULO, ID_ENDERECO_ORIGEM, ID_ENDERECO_DESTINO)
+VALUES (1000.00, '2018-01-01', '2018-02-02', 1, 2, 7, 1, 1, 1, 2),
+	   (5000.00, '2018-04-04', '2018-06-06', 2, 3, 8, 2, 2, 2, 3),
+	   (4500.00, '2018-06-06', '2018-07-07', 1, 1, 9, 3, 3, 1, 4),
+	   (8000.00, '2018-06-06', '2018-08-08', 1, 3, 10, 4, 4, 2, 5),
+	   (7200.00, '2018-07-07', '2018-08-08', 3, 4, 7, 5, 5, 4, 5),
+	   (3500.00, '2018-08-08', '2018-09-09', 2, 2, 7, 1, 1, 1, 3);
